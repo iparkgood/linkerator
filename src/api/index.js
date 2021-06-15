c982a4b56c06f5bb40cad455b3920bf357175969
Koby Johnson <knj11@vt.edu>
2021-06-15 19:23:43 -0400

    Adding a get All Links request

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
-export async function getLinks() {
+const URL = `http://localhost:5000/api`
+
+export async function getAllLinks() {
   try {
-    const { data } = await axios.get('/api');
+    const { data } = await axios.get(URL + `/links`);
     return data;
   } catch (error) {
     throw error;
