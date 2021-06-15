847f72ada5906670a7574b2489b8a968dd83f38b
Koby Johnson <knj11@vt.edu>
2021-06-15 19:24:15 -0400

    Adding a NavBar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,52 +1,28 @@
 import React, { useState, useEffect } from 'react';
-import { AppBar, Toolbar, InputBase } from '@material-ui/core';
-import { fade, makeStyles } from '@material-ui/core/styles';
 
-// import {
-//   getSomething
-// } from '../api';
+import {
+  getAllLinks
+} from '../api';
 
-const useStyles = makeStyles((theme) => ({
-  flex: {
-    display: "flex",
-    justifyContent: "space-between"
-  },
-  searchBar: {
-    position: "relative",
-    borderRadius: "5px",
-    backgroundColor: fade("rgb(0,0,0)", 0.15),
-    paddingLeft: "8px",
-  },
-  searchInput: {
-    color: "inherit"
-  },
-}))
+import NavBar from "./NavBar"
 
 
 const App = () => {
-  const classes = useStyles()
   const [message, setMessage] = useState('');
 
-  // useEffect(() => {
-  //   getSomething()
-  //     .then(response => {
-  //       setMessage(response.message);
-  //     })
-  //     .catch(error => {
-  //       setMessage(error.message);
-  //     });
-  // });
+  useEffect(() => {
+    getAllLinks()
+    // .then(response => {
+    //   setMessage(response.message);
+    // })
+    // .catch(error => {
+    //   setMessage(error.message);
+    // });
+  });
 
   return (
     <div className="App">
-      <AppBar>
-        <Toolbar className={classes.flex}>
-          <h1>The Great Linkerator</h1>
-          <div className={classes.searchBar}>
-            <InputBase className={classes.searchInput} placeholder="Search..." />
-          </div>
-        </Toolbar>
-      </AppBar>
+      <NavBar />
       <h2>{message}</h2>
     </div>
   );
---
+++ src/components/NavBar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { AppBar, Toolbar, InputBase } from '@material-ui/core';
+import { fade, makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  flex: {
+    display: "flex",
+    justifyContent: "space-between"
+  },
+  searchBar: {
+    position: "relative",
+    borderRadius: "5px",
+    backgroundColor: fade("rgb(0,0,0)", 0.15),
+    paddingLeft: "8px",
+  },
+  searchInput: {
+    color: "inherit"
+  },
+}))
+
+const NavBar = () => {
+  const classes = useStyles()
+
+  return (
+    <AppBar>
+      <Toolbar className={classes.flex}>
+        <h1>The Great Linkerator</h1>
+        <div className={classes.searchBar}>
+          <InputBase className={classes.searchInput} placeholder="Search..." />
+        </div>
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+export default NavBar
