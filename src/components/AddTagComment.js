0c94e0b0a5b214f1bbde5ea8f2338dbbd397e6ac
iparkgood <zuzu8723@gmail.com>
2021-06-19 13:55:12 -0400

    new component for a field and button to add a tag/comment

---
+++ src/components/AddTagComment.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+import { TextField, Button } from "@material-ui/core";
+import SendIcon from "@material-ui/icons/Send";
+
+import { createNewTag } from "../api";
+
+const AddTagComment = ({ setTags, linkId, setTagOpen }) => {
+  const [newTag, setNewTag] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleCreateNewTag = async () => {
+    const result = await createNewTag(linkId, newTag);
+
+    setTags((currentTags) => [...currentTags, result]);
+    setTagOpen((currentOpen) => !currentOpen);
+    setNewTag("");
+  };
+
+  return (
+    <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+      <TextField
+        label="Add Tag"
+        value={newTag}
+        onChange={(e) => setNewTag(e.target.value)}
+      />
+      <Button color="secondary" onClick={handleCreateNewTag}>
+        <SendIcon />
+      </Button>
+    </form>
+  );
+};
+
+export default AddTagComment;
