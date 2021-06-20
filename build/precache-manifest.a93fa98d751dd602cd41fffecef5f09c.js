bd62fa9bbf17122ce83a4d4f08cac9b0a05e1165
Koby Johnson <knj11@vt.edu>
2021-06-20 14:19:30 -0400

    adding build folder

---
+++ build/precache-manifest.a93fa98d751dd602cd41fffecef5f09c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57e7a5fc3de6af680339caedf1978c75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ef0e950516a9bd8c589",
+    "url": "/static/js/2.32ddd913.chunk.js"
+  },
+  {
+    "revision": "b948d2a7727b83687a00ec53ef5267c0",
+    "url": "/static/js/2.32ddd913.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77df0d9a90a2aa5ccd4e",
+    "url": "/static/js/main.50e0f957.chunk.js"
+  },
+  {
+    "revision": "7e23c3461cd3b1e0abd6",
+    "url": "/static/js/runtime-main.da5551cf.js"
+  }
+]);
